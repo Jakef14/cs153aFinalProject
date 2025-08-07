c338d623817a32dd3f83868a50f5aa1cd44f3978
Jakef14 <jsf02468@gmail.com>
2025-08-06 21:47:22 -0400

    v5 only

--- CameraAppV1/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TabSelector from './components/TabSelector';
-
-export default function App() {
-  return (
-    <TabSelector/>
-  );
-}
--- CameraAppV2/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TabSelector from './components/TabSelector';
-
-export default function App() {
-  // insert camera permissions here
-  return (
-    <TabSelector/>
-  );
-}
